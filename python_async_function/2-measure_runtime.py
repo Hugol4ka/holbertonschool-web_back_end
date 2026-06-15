eceb1e2a727ed8e3c56985a4fe106f536d1c9c93
Hugol4ka <hugolaca12300@gmail.com>
2026-06-15 14:50:04 +0200

    Fix for PEP8

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -9,6 +9,7 @@ import time
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
+
 def measure_time(n: int, max_delay: int) -> float:
     """
     Mesure le temps d'exécution total de wait_n(n, max_delay)
