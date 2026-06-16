4d9beee3792a4102b7df11a76845676972916e80
Hugol4ka <hugolaca12300@gmail.com>
2026-06-16 13:41:09 +0200

    Fix

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import asyncio
+import random
+from typing import AsyncGenerator
+
+async_generator = __import__('0-asyc_generator').async_generator
+
+async def async_comprehension() -> AsyncGenerator[]
